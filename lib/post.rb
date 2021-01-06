b8c9f475fa3b2d8e6bc6e25f25b546f20a647a46
Ben Hayes <b.t.hayes@comcast.net>
2021-01-06 20:29:58 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,5 +6,15 @@ class Post
   def initialize(title)
     @title = title
     @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end
+  
+  def author_name
+    if self.author
+      self.author.name 
+    end
   
 end
